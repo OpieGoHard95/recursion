c7883932537c3c82c7984673898d1f349c7fc880
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-01-24 22:47:44 -0500

    Problem solved and negative fixed!

---
+++ recursion.js
@@ -0,0 +1,11 @@
+var isEven = function(number) {
+  if (number == 0)
+    return true;
+  else if (number == 1)
+    return false;
+  else if (number > 0)
+    return isEven(number - 2);
+  else if (number < 0)
+    return isEven(number + 2);
+}
+console.log(isEven(-1))
